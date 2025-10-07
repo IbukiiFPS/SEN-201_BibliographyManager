b6801dc53c3bf8256a08034082292c49b446f6fb
PrimWong <rwongkr@cmkl.ac.th>
2025-10-07 17:02:02 +0700

    create entries

---
+++ bibliography_manager/db.py
@@ -0,0 +1,29 @@
+import sqlite3
+from datetime import datetime
+
+DB_FILE = "bibliography.db"
+
+class BibliographyDB:
+    def __init__(self, db_file: str = DB_FILE):
+        self.conn = sqlite3.connect(db_file)
+        self.conn.execute("PRAGMA foreign_keys = ON;")
+        self._create_tables()
+
+    def _create_tables(self):
+        c = self.conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS entries (
+                id INTEGER PRIMARY KEY,
+                authors TEXT NOT NULL,
+                title TEXT NOT NULL,
+                venue TEXT,
+                year INTEGER,
+                volume TEXT,
+                number TEXT,
+                pages TEXT,
+                doi TEXT,
+                url TEXT,
+                tags TEXT,
+                created_at TEXT NOT NULL
+            )''')
+        self.conn.commit()

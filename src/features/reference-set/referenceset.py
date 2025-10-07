5b1aafc96ebcec4f5a1ae5520c0a49f0c6b48d22
PrimWong <rwongkr@cmkl.ac.th>
2025-10-07 14:56:42 +0700

    list and delete

--- src/features/reference-set/referenceset.py
+++ src/features/reference-set/referenceset.py
@@ -7,3 +7,9 @@ class RefSetsService:
         if not name:
             raise ValueError("Set name cannot be empty")
         return self.db.create_refset(name)
+    
+    def list(self) -> List[Tuple]:
+        return self.db.list_refsets()
+    
+    def delete(self, refset_id: int) -> None:  
+        self.db.delete_refset(refset_id)
